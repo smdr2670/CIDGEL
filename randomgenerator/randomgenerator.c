559c72bf80f5fd62aa2bb680a4b0b8c32c38c736
Daniel <daniel.rd@hotmail.de>
2014-06-24 17:13:58 +0200

    Zufalls Genrator weiter verfeinert

--- randomgenerator/randomgenerator.c
+++ randomgenerator/randomgenerator.c
@@ -1,58 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 
 
-int main(){
+int main(int argc, char* argv[]){
 
     srand(time(NULL));
 
+    int rows ;
+    int cols ;
+    int dim ;
+    char *ofname;
+
+    sscanf (argv[1], "%i", &cols);
+    sscanf (argv[2], "%i", &rows);
+    sscanf (argv[3], "%i", &dim);
+    ofname = argv[4];
+ 
+
+    /*
+    if(argc != 4){
+        fprintf(stderr, "l2print");
+    }
+    */
+    
     int i;
     int j;
-    int result;
-	char *ofname=0;
-	FILE *outfile;
-	int n;
-	int k;
-	int dim;
-	char name[80];
-	printf("This program generates a random Generator matrix for a linear [n,k] Code.\n");
-	printf("Please enter n:");
-	scanf("%d",&n);
-	printf("Please enter k:");
-	scanf("%d",&k);
-	if(n <= k){
-		printf("Error! n must be greater than k");
-		exit(1);
-	}
-	printf("Please enter dimension:");
-	scanf("%d",&dim);
-	printf("Now the outputname: ");
-	scanf("%s",name);
+    int k;
+
+    int zerocounter;
+    int border;
+    int counter;
+
+    // Matrix as 1 dimensional array
+    int *mat = (int *)malloc(rows * cols * sizeof(int));
+
+    int offset;
+    // now mat[offset] corresponds to m(i, j)
 
 	/* open outfile */
+    /*
     if (name!=0 && (outfile=fopen(name,"w"))==0){
         fprintf(stderr,"  couldn't open %s for output\n",ofname);
         exit(1);
     }
+    */
 
-    fprintf(outfile, "M: { %d %d %d :\n",k,n,dim);
-    for(i=0;i<k;i++){
-    	for(j=0;j<n;j++){
-    		if(k > j){
-    			if(i == j){
-    				fprintf(outfile, "1 ");
+    //m(i,j)
+    for(i=0;i<rows;i++){
+    	for(j=0;j<cols;j++){
+            offset = i * cols + j;
+            /*Identity Matrix */
+    		if(rows > j){
+                if(i == j){
+                    mat[offset] = 1;
     			}else{
-    				fprintf(outfile, "0 ");
+                    mat[offset] = 0;
     			}
+            /* Generate random */
     		}else{
-                
-                result = (rand()%(dim));
-    			fprintf(outfile, "%d ", result );
-    		}
+                mat[offset] = (rand()%(dim));
+            }
     	}
-    	fprintf(outfile,"\n");
     }
-    fprintf(outfile, "}" );
+    
+    /* checks if the column has a nonzero column vector */
+    for(j=rows+1;j<cols;j++ ){
+        counter = 0;
+        for(i=0;i<rows;i++){
+            offset = i * cols + j;
+            if(mat[offset]==0){
+                zerocounter++;
+            }
+        }
+        if(counter == rows){
+            border = ((rand()%rows)+1)%rows;
+            for(k=0;k<border;k++){
+                offset = i * cols +k;
+                mat[offset] = dim-1;
+            }
+        }
+    }
+
+
+    
+    /* Print matrix */
+    for(i=0;i<rows;i++){
+        for(j=0;j<cols;j++){
+            offset = i * cols + j;
+            printf("%d", mat[offset]);
+        }
+        printf("\n");
+    }
+
+
+    free(mat);
+}
+
+
+void printMatrix(){
+
+
 }
