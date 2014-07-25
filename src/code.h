988aaf2c5a8aa06fe16354c5d603c36f7223746a
smdr2670 <daniel.rd@hotmail.de>
2014-07-25 15:31:47 +0200

    Zwischencommit
    Wieder hochladen

---
+++ src/code.h
@@ -0,0 +1,16 @@
+
+
+#ifndef CODE_H
+#define CODE_H
+
+/**
+ * @brief Given an mxn integer matrix M, compute an rgb for the code ideal I_C.
+ *        Expects generator matrix in standard form, reads off equations for a generating system, then
+ *        uses Buchberger algorithm.
+ * @param M input Matrix
+ * @param m number of rows
+ * @param n number of columns
+ */
+gset gset_code_ideal(int **M,int m, int n);
+
+#endif
--- src/toric.c
+++ src/toric.c
@@ -1,40 +1,36 @@
-/*
-** toric.c                                 Birk Huber, 4/99 
-** -- support for computing first grobner bases of toric ideals.
-** -- includes functions gset_rlex_rgb() and gset_compute_colon()
-**    which once tested and working will move back to the gset.c file. 
-**
-**
-** TiGERS,  Toric Groebner Basis Enumeration by Reverse Search 
-** copyright (c) 1999  Birk Huber
-**
+/** 
+*   @file code.h
+*   @brief Support for computing first grobner bases of code ideals.
+*          of the TiGERS paper.
+*   
+*   This file is reused from TiGERS,
+*   Toric Groebner Basis Enumeration by Reverse Search 
+*   copyright (c) 1999  Birk Huber
+*
+*   @author Birk Huber, 4/99 
+*   @author Daniel Rembold
+*   @bug No known bugs
+*
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "utils.h"
 #include "gset.h"
 #include "matrices.h"
 #include "Rsimp.h"
+#include "code.h"
 
 FILE *outfile;
 
 
 
-/*
-** void gset_compute_colon(gset g,int lv):
-**    Compute deg rev lex rgb (lv least) for colon of g with variable lv.
-**    - set ring_lv=lv.
-**    - convert g to rgb wrt deg_rev_lex as implemented in binomial.h.
-**    - divide each binomial by highest power of var lv possible.
-**    - autoreduce.
-**
-*/
 #define min(m,n) (((m)<(n))?(m):(n))
 
 
 
 /**
- * @brief gset_code_ideal Given an mxn integer matrix M -- compute an rgb for the code ideal I_C.
+ * @brief Given an mxn integer matrix M, compute an rgb for the code ideal I_C.
  *        Expects generator matrix in standard form, reads off equations for a generating system, then
  *        uses Buchberger algorithm.
  * @param M input Matrix
@@ -57,34 +53,9 @@ gset gset_code_ideal(int **M,int m, int n){
     }
 
 
+  
 
-    
-
-    /*
-    fprintf(outfile,"Debug Matrixes, Corank: %d \n", crk);
-    print_imatrix(outfile,debug_str,S,n,m);
-    fprintf(outfile,"\n");
-    print_imatrix(outfile,debug_str,V,n,n);
-   */
-
-    /* create new gset and read off equations corresponding to rows of V*/
-    /*
-    g=gset_new();
-    for(i=crk;i<n;i++){
-        b=binomial_new();
-        for(j=0;j<n;j++){
-            jk=IMref(V,i,j);
-            if (jk>0){
-                binomial_lead(b)[j]=jk;
-                binomial_trail(b)[j]=0;
-            }else {
-                binomial_trail(b)[j]=-1*jk;
-                binomial_lead(b)[j]=0;
-            }
-        }
-        gset_insert(g,b);
-    }
-    */
+ 
 
     // read equations of generator matrix */
 
@@ -113,8 +84,7 @@ gset gset_code_ideal(int **M,int m, int n){
 
 
     /* compute grobner basis wrt to this grade lex term order */
-    // FIXME: lex basis can read off of the matrix with standard-form
-    //        With exhaustive search, lex basis is not good enough
+
     //if(degree_comp == TRUE){
     //gset_rgb(g,monomial_grlexcomp);
     //}
